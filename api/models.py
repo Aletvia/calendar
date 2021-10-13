1e3ef29b3020f3825804a1a4f8d8f31bc9684779
Aletvia Lecona <aletvialecona@gmail.com>
2021-10-13 11:24:43 -0500

    Property, activity and survey models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,66 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+"""
+Abstract model which represent the important dates and state from the objects p. ex.
+    YYYY-MM-DD HH:MM:SS,
+    YYYY-MM-DD HH:MM:SS,
+    active
+"""
+class StateCommonInfo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now_add=True, null=False)
+    status = models.CharField(max_length=35, null=False)
+
+    class Meta:
+        abstract = True
+
+
+"""
+Model which represent a property p. ex.
+    Title,
+    Address complete,
+    Description,
+    YYYY-MM-DD HH:MM:SS,
+    YYYY-MM-DD HH:MM:SS,
+    YYYY-MM-DD HH:MM:SS,
+    active
+"""
+class Property(StateCommonInfo):
+    title = models.CharField(max_length=255, null=False)
+    address = models.TextField(null=False)
+    description = models.TextField(null=False)
+    disabled_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+"""
+Model which represent a activity p. ex.
+    id_property,
+    YYYY-MM-DD HH:MM:SS,
+    Title,
+    YYYY-MM-DD HH:MM:SS,
+    YYYY-MM-DD HH:MM:SS,
+    active
+"""
+class Activity(StateCommonInfo):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    schedule = models.DateTimeField(auto_now_add=True, null=False)
+    title = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+            return self.title
+
+
+"""
+Model which represent a survey p. ex.
+    id_activity,
+    {'question':'answer'},
+    YYYY-MM-DD
+"""
+class Survey(models.Model):
+    activity = models.OneToOneField(Activity, on_delete=models.CASCADE)
+    answers = JSONField(default={}, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
